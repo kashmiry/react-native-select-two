4a571c9deee744661a262a2e8027f701cfcf84f7
kashmiry <mohammed.kashmiry@gmail.com>
2019-12-23 16:14:00 +0300

    Adding image to tag item
    Pass imageComponent prop to TagItem component.

--- index.js
+++ index.js
@@ -246,7 +246,9 @@ class Select2 extends Component {
                                                         this.setState({ data, preSelectedItem });
                                                         onRemoveItem && onRemoveItem(selectedIds, selectedObjectItems);
                                                     }}
-                                                    tagName={tag.name} />
+                                                    tagName={tag.name}
+                                                    tagImage={() => (imageComponent && tag.image) && imageComponent(tag.image)}
+                                                    />
                                             );
                                         })
                                     }
